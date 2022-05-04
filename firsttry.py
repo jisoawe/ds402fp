c5a279fecf54240913cabeb266b13203be9f4276
Jiso Awe <56080860+jisoawe@users.noreply.github.com>
2022-05-04 09:36:36 -0400

    Update firsttry.py
    added the select box

--- firsttry.py
+++ firsttry.py
@@ -1,3 +1,11 @@
-$ pip install streamlit
+import streamlit as st
 
-$ streamlit hello
+st.title('Price of Anarchy Implementation')
+st.header('Jiso Awe & Thomas Schindler')
+st.write('This is our interface for our Data Science 402 final project.')
+
+option = st.selectbox(
+'What type of matrix would you like to test prisoners dilemma on?',
+('1x1', '2x2'))
+
+st.write('You selected:', option)
